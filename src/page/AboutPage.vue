d13563002d8d2def2458b7a128de9d6f2509355f
nasim87 <nasimcu94@gmail.com>
2020-04-26 00:19:45 +0600

    AboutPaqge layout modified

--- src/page/AboutPage.vue
+++ src/page/AboutPage.vue
@@ -4,51 +4,20 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12">
-					<div class="row">
-						<div class="col-md-4">
-							<div class="card mt-2 mb-5">
-								<div class="card-header">
-									<h3>About Page</h3>
-								</div>
-								<div class="card-body">
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-									tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-									quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-									consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-									cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-									proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-								</div>
-							</div>
+					<div class="about-image">
+					<img src="https://indiansolution.com/wp-content/uploads/2020/01/about-us.png" width="100%" height="auto" alt="">
+				</div>
+					<div class="card mt-2 mb-5">
+						<div class="card-header">
+							<h3>About Us</h3>
 						</div>
-						<div class="col-md-4">
-							<div class="card mt-2 mb-5">
-								<div class="card-header">
-									<h3>About Page</h3>
-								</div>
-								<div class="card-body">
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-									tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-									quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-									consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-									cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-									proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-								</div>
-							</div>
-						</div>
-						<div class="col-md-4">
-							<div class="card mt-2 mb-5">
-								<div class="card-header">
-									<h3>About Page</h3>
-								</div>
-								<div class="card-body">
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-									tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-									quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-									consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-									cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-									proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-								</div>
-							</div>
+						<div class="card-body">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 						</div>
 					</div>
 				</div>
@@ -71,5 +40,9 @@ export default {
   }
 };
 </script>
-<style scoped>	
+<style scoped>
+.about-image{
+	width:100%;
+	height: auto;
+}	
 </style>
